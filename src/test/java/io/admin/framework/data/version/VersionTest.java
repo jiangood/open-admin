7bb958674eee00ace9ffee99a2a118f567689a5c
mxvc <410518072@qq.com>
2025-11-12 20:30:00 +0800

    init

--- src/test/java/io/admin/framework/data/version/VersionTest.java
+++ src/test/java/io/admin/framework/data/version/VersionTest.java
@@ -5,24 +5,43 @@ import cn.hutool.core.util.RandomUtil;
 import io.admin.modules.system.dao.SysUserDao;
 import io.admin.modules.system.entity.SysUser;
 import jakarta.annotation.Resource;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 public class VersionTest {
 
+    public static final String ACCOUNT = "test-" + IdUtil.fastUUID();
     @Resource
     private SysUserDao sysUserDao;
 
+
+    @BeforeEach
+    @Transactional
+    public void init() {
+        SysUser user = new SysUser();
+        user.setAccount(ACCOUNT);
+        user = sysUserDao.saveAndFlush(user);
+        System.out.println(user);
+    }
+
     @Test
     public void test() {
-        SysUser user = new SysUser();
-        user.setAccount("test-"+ RandomUtil.randomString(30));
-        user = sysUserDao.saveAndFlush(user);
-        System.out.println("当前版本："+user.getLockVersion());
+        SysUser user = sysUserDao.findByAccount(ACCOUNT);
+        Assertions.assertEquals(0,user.getLockVersion());
+
         user.setExtra3(RandomUtil.randomString(12));
-        sysUserDao.save(user);
-        System.out.println("修改后版本："+user.getLockVersion());
+        user = sysUserDao.save(user);
+        Assertions.assertEquals(1,user.getLockVersion());
+
+
+        user.setExtra3(RandomUtil.randomString(12));
+        user = sysUserDao.saveAndFlush(user);
+        Assertions.assertEquals(2,user.getLockVersion());
 
     }
 
