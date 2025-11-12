8751aab0dfda850a8e4b13fe1baedbce916db158
jiangtao <jiangtao.cctv@qq.com>
2025-11-12 11:08:13 +0800

    init

--- src/main/java/io/admin/AdminFrameworkApplication.java
+++ src/main/java/io/admin/AdminFrameworkApplication.java
@@ -3,10 +3,10 @@ package io.admin;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringApplication {
+public class AdminFrameworkApplication {
 
     public static void main(String[] args) {
-        org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
+        org.springframework.boot.SpringApplication.run(AdminFrameworkApplication.class, args);
     }
 
 }
