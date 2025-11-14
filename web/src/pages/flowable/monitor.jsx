342433e677407cd9d2340f6a7cd169c9e505a57f
jiangtao <jiangtao.cctv@qq.com>
2025-11-14 14:29:21 +0800

    init

--- web/src/pages/flowable/monitor.jsx
+++ web/src/pages/flowable/monitor.jsx
@@ -9,8 +9,8 @@ export default class extends React.Component {
 
   render() {
     const items = [
-      { label: '所有流程', key: 'AllInstance', children: <AllInstance /> },
-      { label: '所有任务', key: 'AllTask', children: <AllTask /> },
+      { label: '运行中的流程', key: 'AllInstance', children: <AllInstance /> },
+      { label: '运行中的任务', key: 'AllTask', children: <AllTask /> },
       { label: '所有定义', key: 'AllDefinition', children: <AllDefinition /> },
     ];
     return <Page padding>  <Tabs items={items} destroyOnHidden /></Page>
