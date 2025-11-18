b4ec420e2bc233b5d31c92890d36a254edeace87
jiangtao <jiangtao.cctv@qq.com>
2025-11-18 10:27:01 +0800

    init

--- src/main/java/io/admin/modules/flowable/core/dto/TaskHandleType.java
+++ src/main/java/io/admin/modules/flowable/core/dto/TaskHandleType.java
@@ -1,16 +1,14 @@
 package io.admin.modules.flowable.core.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum TaskHandleType {
 
     APPROVE("同意"), REJECT("不同意"), BACK("退回");
 
     String message;
 
-
-    TaskHandleType(String message) {
-        this.message = message;
-    }
 }
