a580094dcb5cef80fee867269d5fc6df1585171b
jiangood <410518072@qq.com>
2025-12-24 14:07:46 +0800

    修改message为holder模式

--- web/src/pages/flowable/instance/view.jsx
+++ web/src/pages/flowable/instance/view.jsx
@@ -1,13 +1,15 @@
 import InstanceInfo from "../InstanceInfo";
-import {PageUtils} from "../../../framework";
+import {Page, PageUtils} from "../../../framework";
 import React from "react";
 
 export default class extends React.Component{
 
     render() {
         let params = PageUtils.currentParams();
-        const businessKey = params.businessKey
-        return <InstanceInfo businessKey={businessKey}/>
+        const {businessKey} = params
+        return <Page padding>
+            <InstanceInfo businessKey={businessKey} />
+        </Page>
     }
 
 }
