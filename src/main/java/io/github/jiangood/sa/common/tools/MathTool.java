d10a03a226d8e2d6a8fb5f3c51a8b05cace7fc7d
jiangood <410518072@qq.com>
2026-01-06 16:12:57 +0800

    fix:流程

--- src/main/java/io/github/jiangood/sa/common/tools/MathTool.java
+++ src/main/java/io/github/jiangood/sa/common/tools/MathTool.java
@@ -1,13 +1,47 @@
 package io.github.jiangood.sa.common.tools;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class MathTool {
 
 
     public static String percentStr(Number n, Number total) {
-        double percent = n.doubleValue() / total.doubleValue();
-        String fixed2 = DecimalTool.toFixed2(percent * 100);
-        return fixed2 + "%";
+        return percentStr(n, total, 2);
     }
 
+    public static String percentStr(Number n, Number total,int decimalPlaces) {
+        double percent = n.doubleValue() / total.doubleValue();
+        double v = percent * 100;
 
+        return formatNumberQuick(v, decimalPlaces);
+    }
+
+    /**
+     * 快速格式化（使用String.format，更简洁）
+     * @param number 要格式化的数字
+     * @param decimalPlaces 要保留的小数位数
+     * @return 格式化后的字符串
+     */
+    public static String formatNumberQuick(double number, int decimalPlaces) {
+        if (decimalPlaces < 0) {
+            throw new IllegalArgumentException("小数位数不能为负数: " + decimalPlaces);
+        }
+
+        if (decimalPlaces < 0) {
+            throw new IllegalArgumentException("小数位数不能为负数: " + decimalPlaces);
+        }
+
+        // 先四舍五入到最大小数位数
+        BigDecimal bd = BigDecimal.valueOf(number);
+        bd = bd.setScale(decimalPlaces, RoundingMode.HALF_UP);
+
+        // 直接使用stripTrailingZeros去除末尾0，然后转字符串
+
+        return bd.stripTrailingZeros().toPlainString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(percentStr(1, 5));
+    }
 }
