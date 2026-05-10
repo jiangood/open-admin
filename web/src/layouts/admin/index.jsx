bccc111d25c76e9c4275208b556fd7cf3ea50b32
jiangood <jiangood@outlook.com>
2026-05-10 15:46:36 +0800

    feat: Spin 替换为 Skeleton 骨架屏，提升加载体验

--- web/src/layouts/admin/index.jsx
+++ web/src/layouts/admin/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Badge, Layout, Menu, Spin, Watermark} from 'antd';
+import {Badge, Layout, Menu, Skeleton, Watermark} from 'antd';
 
 import {history, Link} from 'umi';
 import "./index.less"
@@ -135,7 +135,7 @@ export default class extends React.Component {
 
     renderLeftMenu() {
         if(this.state.menuLoading){
-            return <Spin />
+            return <div style={{padding: 16}}><Skeleton active title={false} paragraph={{rows: 8}} /></div>;
         }
         return <Menu items={this.state.menuTree}
                      theme='dark'
@@ -156,7 +156,7 @@ export default class extends React.Component {
     renderCenterContent = () => {
         const {siteInfo, loginInfo} = this.state
         if (this.state.menuTree.length === 0) { // 加载菜单中
-            return <></>
+            return <Skeleton active paragraph={{rows: 10}} style={{padding: 24}} />
         }
         const tabPageRenderNode = <TabPageRender pathMenuMap={this.state.pathMenuMap}/>;
         if (siteInfo.waterMark === true) {
