60d6304634b4a0c55176378561879035d6259bbc
姜涛 <410518072@qq.com>
2026-05-15 20:28:34 +0800

    重构: 将 web/src/loading.jsx 转换为函数式组件

--- web/src/loading.jsx
+++ web/src/loading.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-import {Spin} from "antd";
-import {ThemeUtils} from "./framework";
+import { Spin } from "antd";
+import { ThemeUtils } from "./framework";
 
-export default class extends React.Component {
-    render() {
-        return <div
-            style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                height: '100vh',
-                background: ThemeUtils.getColor("background-color")
-            }}>
-            <Spin size='large'/>
-        </div>
-    }
-}
+const Loading = () => (
+  <div
+    style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100vh',
+      background: ThemeUtils.getColor("background-color")
+    }}
+  >
+    <Spin size='large' />
+  </div>
+);
+
+export default Loading;
