5cb4b980209bb07bf2e91c31a5b518f4209f4d90
姜涛 <410518072@qq.com>
2026-05-15 20:31:31 +0800

    refactor: 重构 ButtonList 为函数式组件，保留 export function，不使用 export default。

--- web/src/framework/components/system/ButtonList.tsx
+++ web/src/framework/components/system/ButtonList.tsx
@@ -1,42 +1,29 @@
 import React from 'react';
-import {Space} from 'antd';
-import {PermUtils} from "../../utils";
-
+import { Space } from 'antd';
+import { PermUtils } from "../../utils";
 
 /**
  * 带权限的按钮列表
  * @param maxNum: 显示子节点的个数， 超过的为收缩起来
- *
  */
-export class ButtonList extends React.Component {
+export function ButtonList(props) {
+  const { children } = props;
 
+  // 检查权限
+  const checkPerm = (element) => {
+    const _props = element?.props;
+    return _props == null || _props.perm == null || PermUtils.hasPermission(_props.perm);
+  };
 
-    render() {
-        const {children} = this.props;
+  // 单节点情况
+  if (!Array.isArray(children)) {
+    return checkPerm(children) ? children : null;
+  }
 
-        // 单节点的情况
-        if (!Array.isArray(children)) {
-            if (this.checkPerm(children)) {
-                return children;
-            }
-            return null
-        }
-
-        const menus = [];
-        for (let child of children) {
-            if (child === null || child === undefined) {
-                continue;
-            }
-            if (this.checkPerm(child)) {
-                menus.push(child);
-            }
-        }
-        return <Space>{menus}</Space>;
-    }
-
-    checkPerm = element => {
-        const props = element.props;
-        return props == null || props.perm == null || PermUtils.hasPermission(props.perm);
-
-    };
+  const menus = [];
+  for (let child of children) {
+    if (child == null) continue;
+    if (checkPerm(child)) menus.push(child);
+  }
+  return <Space>{menus}</Space>;
 }
