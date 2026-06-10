0e666c95fbd9a5e932ddc4238b0e1ce3314bf16f
jiangood <410518072@qq.com>
2026-06-10 12:07:08 +0800

    feat(dict): add deleteByTypeCode to SysDictItemRepository

--- src/main/java/io/github/jiangood/openadmin/modules/system/repository/SysDictItemRepository.java
+++ src/main/java/io/github/jiangood/openadmin/modules/system/repository/SysDictItemRepository.java
@@ -11,5 +11,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SysDictItemRepository extends BaseRepository<SysDictItem, String> {
 
-
+    void deleteByTypeCode(String typeCode);
 }
