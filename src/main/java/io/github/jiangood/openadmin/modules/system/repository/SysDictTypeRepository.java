8ec2d27a51dd90dea75e0ccd0a75a5b60d8bb9d4
jiangood <410518072@qq.com>
2026-06-10 11:56:41 +0800

    feat(dict): add SysDictTypeRepository

---
+++ src/main/java/io/github/jiangood/openadmin/modules/system/repository/SysDictTypeRepository.java
@@ -0,0 +1,9 @@
+package io.github.jiangood.openadmin.modules.system.repository;
+
+import io.github.jiangood.openadmin.framework.data.BaseRepository;
+import io.github.jiangood.openadmin.modules.system.entity.SysDictType;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SysDictTypeRepository extends BaseRepository<SysDictType, String> {
+}
