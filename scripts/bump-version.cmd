d3d0053d4da5c7acb5b8d34125f6edb37f59d91c
jiangood <jiangood@github.com>
2026-07-22 12:40:13 +0800

    chore: remove scripts/ directory (bump-version utility)

--- scripts/bump-version.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0.."
-node scripts/bump-version.js bump pom.xml web/package.json %1
--- scripts/bump-version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-cd "$(dirname "$0")/.."
-node scripts/bump-version.js bump pom.xml web/package.json "$1"
