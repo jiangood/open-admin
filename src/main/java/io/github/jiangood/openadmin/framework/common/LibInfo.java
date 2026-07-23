c71afff6c7b1291502c7ee4ad295ce474e47fc4c
jiangood <jiangood@github.com>
2026-07-23 17:51:12 +0800

    chore: update LibInfo build time

--- src/main/java/io/github/jiangood/openadmin/framework/common/LibInfo.java
+++ src/main/java/io/github/jiangood/openadmin/framework/common/LibInfo.java
@@ -2,7 +2,7 @@ package io.github.jiangood.openadmin.framework.common;
 
 public class LibInfo {
     public static final String VERSION = "2.5.5";
-    public static final String BUILD_TIME = "2026-07-23T09:48:27.639Z";
+    public static final String BUILD_TIME = "2026-07-23T09:50:38.775Z";
 
     private LibInfo() {}
 }
