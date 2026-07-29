29ba7493851346ecfb33db2ed8e12c49158282fd
jiangood <jiangood@github.com>
2026-07-29 17:58:12 +0800

    release: v3.0.3

--- src/main/java/io/github/jiangood/openadmin/framework/auth/dto/LoginInfoVO.java
+++ src/main/java/io/github/jiangood/openadmin/framework/auth/dto/LoginInfoVO.java
@@ -9,7 +9,6 @@ public class LoginInfoVO {
     private String id;
     private String name;
     private String orgName;
-    private String orgName;
     private List<String> permissions;
     private String account;
     private String roleNames;
