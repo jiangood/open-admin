a2379ca6a574c6037935fa44dff7eac09707bd9a
jiangood <jiangood@github.com>
2026-08-04 17:09:35 +0800

    feat: 个人中心移除顶部渐变横幅

--- web/src/pages/userCenter/index.jsx
+++ web/src/pages/userCenter/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Avatar, Card, Descriptions, Space, Tag, Tabs, Typography} from "antd";
+import {Card, Descriptions, Tabs} from "antd";
 import ChangePassword from "./changePassword";
 import PermView from "./permView";
 import {HttpUtils, Page} from "../../framework";
@@ -19,28 +19,6 @@ export default class extends React.Component {
     render() {
         const {info} = this.state;
         return <Page backgroundGray title="个人中心" description="查看和编辑个人资料">
-
-            <Card style={{marginBottom: 16}} styles={{body: {padding: 0}}}>
-                <div style={{
-                    background: 'linear-gradient(135deg, #1677ff 0%, #69b1ff 100%)',
-                    padding: '40px 24px 56px',
-                    textAlign: 'center',
-                }}>
-                    <Avatar size={96} title='点击修改头像' style={{backgroundColor: 'rgba(255,255,255,0.25)', fontSize: 40}}>
-                        {info.name ? info.name.charAt(0) : 'U'}
-                    </Avatar>
-                    <div style={{marginTop: 16}}>
-                        <span style={{fontSize: 22, fontWeight: 600, color: '#fff'}}>{info.name}</span>
-                    </div>
-                    <Typography.Text style={{color: 'rgba(255,255,255,0.85)'}}>账号：{info.account}</Typography.Text>
-                    <div style={{marginTop: 8}}>
-                        <Space wrap>
-                            {(info.roles || []).map((r, i) => <Tag color="blue" key={i}>{r}</Tag>)}
-                            {info.org ? <Tag color="cyan">{info.org}</Tag> : null}
-                        </Space>
-                    </div>
-                </div>
-            </Card>
 
             <Card title="个人信息" style={{marginBottom: 16}}>
                 <Descriptions column={2} size="middle">
